ab93e8e19f65d0e3e2d10cb444b1cdd7fcc19012
hmvictor <hmvictor@localhost>
2010-04-04 22:32:43 -0500

    Initial version of Carmesi

---
+++ main/umbrella/ForwardToView.java
@@ -0,0 +1,18 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package umbrella;
+
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+
+/**
+ *
+ * @author Victor
+ */
+@Retention(RetentionPolicy.RUNTIME)
+public @interface ForwardToView {
+     String value();
+}
