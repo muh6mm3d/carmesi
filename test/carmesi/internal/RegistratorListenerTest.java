2b8c5f3a88a12852c099dce877fe32af7e72e0aa
hmvictor <hmvictor@localhost>
2011-02-20 01:49:01 -0600

    Implemented after redisegn RegistratorListener.

--- test/carmesi/internal/RegistratorListenerTest.java
+++ test/carmesi/internal/RegistratorListenerTest.java
@@ -1,5 +1,14 @@
 package carmesi.internal;
 
+import java.util.List;
+import carmesi.internal.dynamic.DynamicControllerServlet;
+import org.junit.Test;
+import java.util.EnumSet;
+import javax.servlet.DispatcherType;
+import javax.servlet.Filter;
+import javax.servlet.FilterRegistration;
+import javax.servlet.ServletRegistration;
+import javax.servlet.Servlet;
 import carmesi.BeforeURL;
 import carmesi.Controller;
 import carmesi.RedirectTo;
@@ -7,25 +16,27 @@ import carmesi.URL;
 import carmesi.convertion.Converter;
 import carmesi.convertion.ConverterFor;
 import carmesi.convertion.TargetInfo;
+import carmesi.internal.dynamic.DynamicController;
 import java.awt.Point;
 import java.io.BufferedWriter;
 import java.io.File;
-import java.io.FileInputStream;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.util.LinkedList;
 import javax.servlet.ServletContext;
 import javax.servlet.ServletContextEvent;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import org.junit.Rule;
-import org.junit.Test;
 import org.junit.rules.TemporaryFolder;
+import org.mockito.ArgumentCaptor;
 import static org.mockito.Mockito.*;
-import static org.hamcrest.CoreMatchers.*;
 import static org.junit.Assert.*;
+import static org.hamcrest.CoreMatchers.*;
 
 /**
- *
+ * Annotation processing must be disabled.
+ * 
  * @author Victor
  */
 public class RegistratorListenerTest {
@@ -36,32 +47,86 @@ public class RegistratorListenerTest {
     }
 
     @Test
-    public void testContextInitialized() {
-    }
-
-    @Test
-    public void testContextDestroyed() {
+    public void testCreateControllers() throws IOException, ClassNotFoundException, InstantiationException, IllegalAccessException{
+        File file=folder.newFile("carmesi.list");
+        BufferedWriter writer=new BufferedWriter(new FileWriter(file));
+        Class classes[]={TypesafeController.class, TypesafeControllerBefore.class, MyDynamicController.class, MyDynamicControllerBefore.class, MyConverter.class};
+        List<ControllerInfo> infos=new LinkedList<ControllerInfo>();
+        for(Class c:classes){
+            writer.write(c.getCanonicalName());
+            writer.newLine();
+            if(!Converter.class.isAssignableFrom(c)){
+                infos.add(ControllerInfo.createInfo(c));
+            }
+        }
+        writer.close();
+        
+        ServletContext servletContext=mock(ServletContext.class);
+        ControllerFactory factory=spy(new SimpleControllerFactory());
+        
+        ServletRegistration.Dynamic dynamicServlet=mock(ServletRegistration.Dynamic.class);
+        when(servletContext.addServlet(anyString(), (Servlet)any())).thenReturn(dynamicServlet);
+        
+        FilterRegistration.Dynamic dynamicFilter=mock(FilterRegistration.Dynamic.class);
+        when(servletContext.addFilter(anyString(), (Filter)any())).thenReturn(dynamicFilter);
+        
+        RegistratorListener registrator=new RegistratorListener(factory, file.toURI().toURL());
+        registrator.contextInitialized(new ServletContextEvent(servletContext));
+        
+        ArgumentCaptor<DynamicControllerServlet> servletCaptor=ArgumentCaptor.forClass(DynamicControllerServlet.class);
+        ArgumentCaptor<ControllerFilter> filterCaptor=ArgumentCaptor.forClass(ControllerFilter.class);
+        
+        verify(servletContext, times(2)).addServlet(anyString(), (Servlet)any());
+        verify(servletContext, times(2)).addFilter(anyString(), (Filter)any());
+        verify(servletContext).addServlet(eq(MyDynamicController.class.getSimpleName()), servletCaptor.capture());
+        verify(servletContext).addFilter(eq(MyDynamicControllerBefore.class.getSimpleName()), filterCaptor.capture());
+        
+        DynamicController controller=(DynamicController) servletCaptor.getValue().getController();
+        DynamicController controllerBefore=(DynamicController) filterCaptor.getValue().getController();
+        
+        assertThat(controller.getConverter(Point.class), is(MyConverter.class));
+        assertThat(controllerBefore.getConverter(Point.class), is(MyConverter.class));
+        EnumSet<DispatcherType> set = EnumSet.of(DispatcherType.REQUEST);
+        
+        for(ControllerInfo info:infos){
+            if(info.isBefore){
+                verify(dynamicFilter).addMappingForUrlPatterns(set, false, info.url);
+            }else{
+                verify(dynamicServlet).addMapping(info.url);
+            }
+        }
+        
+        for(Class c:classes){
+            if(!Converter.class.isAssignableFrom(c)){
+                verify(factory).createController(c);
+            }
+        }
     }
     
-    public void testCreateControllers() throws IOException, ClassNotFoundException, InstantiationException, IllegalAccessException{
-        throw new UnsupportedOperationException("Not yet implemented");
-//        File file=folder.newFile("carmesi.list");
-//        BufferedWriter writer=new BufferedWriter(new FileWriter(file));
-//        Class classes[]={TypesafeController.class, TypesafeControllerBefore.class, MyDinamicController.class, MyConverter.class};
-//        for(Class c:classes){
-//            writer.write(c.getName());
-//            writer.newLine();
-//        }
-//        writer.close();
-//        FileInputStream input=new FileInputStream(file);
-//        
-//        RegistratorListener registrator=new RegistratorListener();
-//        ServletContext servletContext=mock(ServletContext.class);
-//        ControllerFactory factory=mock(SimpleControllerFactory.class);
-//        registrator.contextInitialized(new ServletContextEvent(servletContext));
-//        registrator.addClasses(input);
-//        registrator.setControllerFactory(factory);
-//        input.close();
+    static class ControllerInfo{
+        private String classCanonicalName;
+        private String url;
+        private boolean isBefore;
+
+        public ControllerInfo(String classCanonicalName, String url) {
+            this.classCanonicalName = classCanonicalName;
+            this.url = url;
+        }
+        
+        public static ControllerInfo createInfo(Class<Object> controllerClass){
+            String url=null;
+            boolean before=false;
+            if(controllerClass.isAnnotationPresent(URL.class)){
+                url=controllerClass.getAnnotation(URL.class).value();
+            }else if(controllerClass.isAnnotationPresent(BeforeURL.class)){
+                url=controllerClass.getAnnotation(BeforeURL.class).value();
+                before=true;
+            }
+            ControllerInfo controllerInfo = new ControllerInfo(controllerClass.getCanonicalName(), url);
+            controllerInfo.isBefore=before;
+            return controllerInfo;
+        }
+        
     }
     
     @URL("/typesafe")
@@ -85,7 +150,7 @@ public class RegistratorListenerTest {
     
     @URL("/dynamic")
     @RedirectTo("/dynamicView.jsp")
-    public static class MyDinamicController{
+    public static class MyDynamicController{
 
         public void doAction(){
             
@@ -94,7 +159,7 @@ public class RegistratorListenerTest {
     }
     
     @BeforeURL("/dynamicBefore")
-    public static class MyDinamicControllerBefore{
+    public static class MyDynamicControllerBefore{
 
         public void doAction(){
             
