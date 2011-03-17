73539fc862f63cef1b199c42aeef694739ee7821
hmvictor <hmvictor@localhost>
2011-03-16 22:16:13 -0600

    Renaming of HttpMethods to AllowedHttpMethods

---
+++ main/carmesi/AllowedHttpMethods.java
@@ -0,0 +1,24 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package carmesi;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+/**
+ * The allowed HTTP methods for invocating a controller.
+ * 
+ * @author Victor Hugo Herrera Maldonado
+ */
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.TYPE)
+public @interface AllowedHttpMethods {
+    
+    HttpMethod[] value();
+    
+}
