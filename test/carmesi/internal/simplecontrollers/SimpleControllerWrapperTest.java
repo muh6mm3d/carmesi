7872ef07ac791b7ced0f0c1cace4700e11b38753
hmvictor <hmvictor@localhost>
2011-03-24 16:45:00 -0600

    Testing of creation of instance when methods are annotated as callbacks or injection setters.

---
+++ test/carmesi/internal/simplecontrollers/SimpleControllerWrapperTest.java
@@ -0,0 +1,149 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package carmesi.internal.simplecontrollers;
+
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
+import javax.annotation.Resource;
+import javax.ejb.EJB;
+import javax.inject.Inject;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author Victor
+ */
+public class SimpleControllerWrapperTest {
+
+    @Test
+    public void shouldCreateInstance() {
+        SimpleControllerWrapper instance =
+            SimpleControllerWrapper.createInstance(new Object() {
+
+                public void execute() {
+                }
+            });
+        assertNotNull(instance);
+    }
+
+    @Test
+    public void shouldCreateInstanceWithCallbackMethods() {
+        SimpleControllerWrapper instance =
+            SimpleControllerWrapper.createInstance(new Object() {
+
+                @PostConstruct
+                public void init() {
+                }
+
+                @PreDestroy
+                public void dispose() {
+                }
+
+                public void execute() {
+                }
+            });
+        assertNotNull(instance);
+    }
+
+    @Test
+    public void shouldCreateInstanceWithResourceInjection() {
+        SimpleControllerWrapper instance =
+            SimpleControllerWrapper.createInstance(new Object() {
+
+                @Resource
+                public void setA(Object a) {
+                }
+
+                public void execute() {
+                }
+            });
+        assertNotNull(instance);
+    }
+
+    @Test
+    public void shouldCreateInstanceWithCDIInjection() {
+        SimpleControllerWrapper instance =
+            SimpleControllerWrapper.createInstance(new Object() {
+
+                @Inject
+                public void setA(Object a) {
+                }
+
+                public void execute() {
+                }
+            });
+        assertNotNull(instance);
+    }
+
+    @Test
+    public void shouldCreateInstanceWithEJBInjection() {
+        SimpleControllerWrapper instance =
+            SimpleControllerWrapper.createInstance(new Object() {
+
+                @EJB
+                public void setA(Object a) {
+                }
+
+                public void execute() {
+                }
+            });
+        assertNotNull(instance);
+    }
+    
+    @Test(expected=IllegalArgumentException.class)
+    public void shouldNotCreateInstance() {
+        SimpleControllerWrapper instance =
+            SimpleControllerWrapper.createInstance(new Object() {
+                
+                @PostConstruct
+                public void init(){
+                    
+                }
+                
+                @PreDestroy
+                public void dispose(){
+                    
+                }
+                
+                @Resource
+                public void setResource(Object o){
+                    
+                }
+                
+                @Inject
+                public void setBean(Object o){
+                    
+                }
+                
+                @EJB
+                public void setEJB(Object a) {
+                    
+                }
+
+                
+            });
+        fail();
+    }
+    
+    
+    @Test(expected=IllegalArgumentException.class)
+    public void shouldNotCreateInstanceToo() {
+        SimpleControllerWrapper instance =
+            SimpleControllerWrapper.createInstance(new Object() {
+                
+                public void execute(){
+                    
+                }
+                
+                public void execute2(){
+                    
+                }
+                
+            });
+        fail();
+    }
+    
+}
